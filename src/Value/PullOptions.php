85dc59f77d99865fe3e343147103673d094eced7
Neel Vora <neelbvora@gmail.com>
2026-04-20 17:27:42 -0500

    options: add PullOptions value object for transfer service

---
+++ src/Value/PullOptions.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\config_pull\Value;
+
+final class PullOptions {
+
+  public function __construct(
+    public readonly string $remoteName,
+    public readonly string $syncDir,
+    public readonly ?string $onlyPattern = NULL,
+    public readonly ?string $excludePattern = NULL,
+    public readonly bool $dryRun = FALSE,
+    public readonly bool $withTranslations = FALSE,
+  ) {}
+
+}
